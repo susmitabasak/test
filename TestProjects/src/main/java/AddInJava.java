094d65df91e23579a5b8a93ceb7c384d9262e32d
Susmita Basak <saha.susmita@gmail.com>
2022-03-16 16:32:29 -0600

    initial commit

---
+++ TestProjects/src/main/java/AddInJava.java
@@ -0,0 +1,7 @@
+public class AddInJava {
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 9;
+        System.out.println("Sum of " + x + " and " + y + " is " + (x+y));
+    }
+}
